15e105b878dcb9f125708eabe6565b27d4660c9c
Mohamed Bastawy <mohamed.bastawiie@gmail.com>
2026-04-15 18:56:24 +0200

    Add ThemeContext and wire theme toggle in header
    Introduce ThemeProvider and useTheme hook; wrap App with ThemeProvider.
    Change HeaderActionButton from a div to a button that accepts onTap, and
    pass
    toggleTheme to the ThemeIcon action.

--- src/components/layout/header/header-actions/HeaderActionButton.jsx
+++ src/components/layout/header/header-actions/HeaderActionButton.jsx
@@ -1,7 +1,11 @@
 import styles from "./HeaderActions.module.css";
 
-const HeaderActionButton = ({ icon }) => {
-  return <div className={styles.icon}>{icon}</div>;
+const HeaderActionButton = ({ icon, onTap }) => {
+  return (
+    <button type="button" onClick={onTap} className={styles.icon}>
+      {icon}
+    </button>
+  );
 };
 
 export default HeaderActionButton;
--- src/components/layout/header/header-actions/HeaderActions.jsx
+++ src/components/layout/header/header-actions/HeaderActions.jsx
@@ -2,15 +2,19 @@ import CartIcon from "/src/assets/cart_icon.svg?react";
 import LangIcon from "/src/assets/lang_icon.svg?react";
 import ThemeIcon from "/src/assets/theme_icon.svg?react";
 
+import { useTheme } from "../../../../context/ThemeContext";
+
 import HeaderActionButton from "./HeaderActionButton";
 
 import styles from "./HeaderActions.module.css";
 
 const HeaderActions = () => {
+  const { _, toggleTheme } = useTheme();
+
   return (
     <div className={styles.icons}>
       <HeaderActionButton icon={<LangIcon />} />
-      <HeaderActionButton icon={<ThemeIcon />} />
+      <HeaderActionButton icon={<ThemeIcon />} onTap={toggleTheme} />
       <HeaderActionButton icon={<CartIcon />} />
     </div>
   );
